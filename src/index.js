2d34dbd0c3e2e8e3f734b3811985dc61354bc944
Valentin <valeprocenko@yandex.ru>
2021-04-23 21:44:02 +0300

    finish base table logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 
+
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
 });

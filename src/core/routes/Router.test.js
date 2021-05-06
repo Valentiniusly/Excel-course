0762c92e482585a1f2a98de410fead34d59eefd2
Valentin <valeprocenko@yandex.ru>
2021-05-06 23:21:01 +0300

    Create unit testing env

---
+++ src/core/routes/Router.test.js
@@ -0,0 +1,33 @@
+import {Page} from '@core/Page';
+import {Router} from '@core/routes/Router';
+
+class DashboardPage extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'dashboard'
+    return root
+  }
+}
+class ExcelPage extends Page {}
+
+describe('Router:', () => {
+  let router
+  let $root
+
+  beforeEach(() => {
+    $root = document.createElement('div')
+    router = new Router($root, {
+      dashboard: DashboardPage,
+      excel: ExcelPage
+    })
+  })
+
+test('should be defined', () => {
+    expect(Router).toBeDefined()
+  })
+
+test('should render page', () => {
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>dashboard</div>')
+  })
+})
